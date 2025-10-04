84dcbd06ada0e3712ba1f0f6d5462d5a98e824ea
Hugo Soares <hugos2000@protonmail.com>
2025-10-04 17:49:41 -0300

    added cauldron mix mechanic (barebones)

--- cauldron.gd
+++ cauldron.gd
@@ -1,5 +1,8 @@
 extends StaticBody2D
 
+var mix_percent: int = 0
+@export var increment: int = 10
+
 @onready var interactable: Area2D = $Interactable
 
 func _ready() -> void:
@@ -7,3 +10,9 @@ func _ready() -> void:
 
 func _on_interact():
 	print("cauldron interacted")
+	mix_cauldron()
+
+func mix_cauldron() -> void:
+	mix_percent += increment
+	mix_percent = clamp(mix_percent, 0, 100)
+	print("Mix:", mix_percent, "%")
