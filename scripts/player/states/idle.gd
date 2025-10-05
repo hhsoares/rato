405030e3a330dd81ab4c4032379cae5a18912dab
Hugo Soares <hugos2000@protonmail.com>
2025-10-05 18:38:08 -0300

    tweaked character size and mov speed

--- scripts/player/states/idle.gd
+++ scripts/player/states/idle.gd
@@ -10,5 +10,5 @@ func physics_update(delta: float) -> void:
 	if player.input_direction != Vector2.ZERO:
 		state_machine.change_state("move")
 
-	player.move(0, 15, delta)
+	player.move(0, 10, delta)
 	player.move_and_slide()
--- scripts/player/states/move.gd
+++ scripts/player/states/move.gd
@@ -8,5 +8,5 @@ func physics_update(delta: float) -> void:
 	if player.input_direction == Vector2.ZERO:
 		state_machine.change_state("idle")
 	
-	player.move(400, 20, delta)
+	player.move(200, 15, delta)
 	player.move_and_slide()
