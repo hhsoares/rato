663315df8f0b906a7d5362edb740bc91dd4544fa
Hugo Soares <hugos2000@protonmail.com>
2025-10-05 13:37:15 -0300

    fixed cauldron mixing mechanic

--- scripts/player/states/mix.gd
+++ scripts/player/states/mix.gd
@@ -1,12 +1,21 @@
-# mix.gd
 extends State
 @onready var player: Player = get_parent().get_parent()
+var cauldron: Cauldron
 
 func enter() -> void:
+	cauldron = get_tree().get_first_node_in_group("cauldron") as Cauldron
 	print("Mix: enter")
 
 func exit() -> void:
 	print("Mix: exit")
 
-func physics_update(_delta: float) -> void:
-	pass
+func physics_update(delta: float) -> void:
+	if not cauldron:
+		return
+	if not cauldron.session_active:
+		return
+	if not Input.is_action_pressed("mix"):
+		return
+	
+	var p := cauldron.mix_step(delta)
+	print("Mix:", int(p), "%")
